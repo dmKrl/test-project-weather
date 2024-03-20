80b794b793872e3fca492386f492c2c0eeb23297
Dmitry Kralichkin <dm_krl@mail.ru>
2024-03-20 14:56:51 +1200

    change page notfound and add packages for test

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,22 @@
+import s from './NotFound.module.css';
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className={s.notFound}>
+      <div className={s.notFoundTitle}>
+        <h2 className={s.notFoundHeading}>404</h2>
+        <img
+          src="/src/assets/images/icons/norfound.svg"
+          alt="not-found-img-rain"
+        />
+      </div>
+      <p className={s.notFoundText}>
+        Что-то пошло не так, попробуйте перезагрузить страницу или вернуться на
+        главную
+      </p>
+      <a href="/">Вернуться на главную</a>
+    </div>
+  );
 };
 
 export default NotFound;
